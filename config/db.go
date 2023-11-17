c914f762431bc6fb9093de255108368bd342a412
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-11-17 23:37:36 +0700

    config

---
+++ config/db.go
@@ -0,0 +1,26 @@
+package config
+
+import(
+	"github.com/Daviskelvin824/OldEgg/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func migrate(){
+	DB.AutoMigrate(&model.User{})
+}
+
+func connect(){
+	psqlInfo := "host=localhost user=postgres password=postgres dbname=oldegg port=5432 TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	DB = db
+
+	migrate()
+}
