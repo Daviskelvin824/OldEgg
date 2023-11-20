650d7bca8acf45f904e10e49c6f09e71ce8376db
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-11-20 13:36:28 +0700

    Create shop.go

---
+++ models/shop.go
@@ -0,0 +1,13 @@
+package models
+
+import "gorm.io/gorm"
+
+type Shop struct {
+	gorm.Model
+	ShopName           string `json:"shop_name"`
+	ShopEmail          string `json:"shop_email"`
+	ShopPassword       string `json:"shop_password"`
+	Status             string `json:"status"`
+	AboutUs            string `json:"about_us"`
+	DisplayPictureLink string `json:"display_picture_link"`
+}
