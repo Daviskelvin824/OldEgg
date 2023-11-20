318326678ea3a3ba6dc133d54a80bc2e44f73a73
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-11-20 13:43:30 +0700

    Create ProductRoute.go

---
+++ route/ProductRoute.go
@@ -0,0 +1,19 @@
+package route
+
+import (
+	"github.com/Daviskelvin824/OldEgg/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func ProductRoute(router *gin.Engine) {
+
+	// router.POST("/create-product", controller.CreateProduct)
+	// router.POST("/get-products", controller.GetProducts)
+	// router.POST("/get-product-by-id", controller.GetProductByID)
+	// router.POST("/update-product", controller.UpdateProduct)
+	// router.POST("/search-product", controller.SearchProduct)
+
+	router.POST("/get-recommended-products", controller.GetRecommendedProducts)
+	// router.POST("/get-recommended-products-by-shop", controller.GetRecommendedProductsByShop)
+
+}
