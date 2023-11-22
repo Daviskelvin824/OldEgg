47c86f84a22633de422f8563ed98eeb177b34066
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-11-22 14:03:40 +0700

    Create searchQuery.go

---
+++ models/searchQuery.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type SearchQuery struct {
+	gorm.Model
+	Email           string `json:"email"`
+	Keyword         string `json:"keyword"`
+	InnerKeyword    string `json:"inner_keyword"`
+	IsAvailableOnly bool   `json:"is_available_only"`
+}
