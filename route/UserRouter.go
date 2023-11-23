05ab3bab927e12103bdf4ee1f4990ed89cb7d277
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-11-23 14:55:29 +0700

    Update UserRouter.go

--- route/UserRouter.go
+++ route/UserRouter.go
@@ -7,9 +7,11 @@ import (
 )
 
 func UserRoute(router *gin.Engine) {
-
+	router.POST("/update-user", controller.UpdateUser)
 	router.POST("/signup", controller.InsertUser)
 	router.POST("/signin", controller.SignIn)
 	router.POST("/authenticate", middleware.RequireAuthentication, controller.Authenticate)
 	router.POST("/subscribe-to-newsletter", controller.SubscribeToNewsletter)
+	router.POST("/request-two-factor-authentication-code", controller.RequestTFACode)
+	router.POST("/change-password", controller.ChangePassword)
 }
