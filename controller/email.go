542ca5ea43ade86fcf9c07c3c40f5e438ef17082
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-11-23 14:33:55 +0700

    Update email.go

--- controller/email.go
+++ controller/email.go
@@ -18,7 +18,7 @@ func BlastEmail(c *gin.Context) {
 	var requestBody RequestBody
 	c.ShouldBindJSON(&requestBody)
 
-	auth := smtp.PlainAuth("", "oldegg2023@gmail.com", "Old.egg@2023oldegg", "smtp.gmail.com")
+	auth := smtp.PlainAuth("", "oldegg2023@gmail.com", "hhxxpsrtsgtidtek", "smtp.gmail.com")
 
 	var subscribedUsers []models.User
 
@@ -61,7 +61,7 @@ func SendEmail(c *gin.Context) {
 	var requestBody RequestBody
 	c.ShouldBindJSON(&requestBody)
 
-	auth := smtp.PlainAuth("", "oldegg2023@gmail.com", "Old.egg@2023oldegg", "smtp.gmail.com")
+	auth := smtp.PlainAuth("", "oldegg2023@gmail.com", "hhxxpsrtsgtidtek", "smtp.gmail.com")
 
 	msg := "Subject: " + requestBody.MailSubject + "\n" + requestBody.MailBody
 	var to []string
