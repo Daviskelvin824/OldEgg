345da7d7727dbd3419cbdea741ac2ccd8e05e251
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-12-01 10:27:46 +0700

    Create ProductCategory.go

---
+++ route/ProductCategory.go
@@ -0,0 +1,15 @@
+package route
+
+import (
+	"github.com/Daviskelvin824/OldEgg/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func ProductCategoryRoute(router *gin.Engine) {
+
+	router.GET("/get-product-categories", controller.GetProductCategories)
+	router.POST("/get-product-category-by-shop-id", controller.GetProductCategoryByShopID)
+	router.GET("/get-popular-product-categories", controller.GetPopularProductCategories)
+
+}
+
