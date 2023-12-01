bacae80e5158897b6b7454545485ff0a7e6ad2de
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-12-01 10:23:50 +0700

    Update requireAuth.go

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -1,41 +1,85 @@
-import authenticate from "../Pages/api-calls/auth/authenticate";
-import getCookie from "../utility/getCookie";
-import { useEffect, useState } from "react";
+package middleware
 
-const useAuth = () => {
+import (
+	"fmt"
+	"os"
+	"time"
 
-  const [user, setUser] = useState({});
+	"github.com/Daviskelvin824/OldEgg/config"
+	"github.com/Daviskelvin824/OldEgg/models"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"gorm.io/gorm"
+)
 
-  useEffect(() => {
+func RequireAuthentication(c *gin.Context) {
 
-    const getUser = async () => {
+	type JWTToken struct {
+		gorm.Model
+		TokenString string `json:"token_string"`
+	}
 
-      const cookie = getCookie("Auth")
-      const result = await authenticate({
-        "token_string": cookie
-      });
-      if (result.role_id) setUser(result);
-      else if (result.shop_name)
-        setUser({
-          id: result.ID,
-          first_name: result.shop_name,
-          email: result.shop_email,
-          password: result.shop_password,
-          role_id: 3,
-          status: result.status
-        })
-      else{
-        setUser({})
-      }
-  
-    }
-  
-    getUser();
+	var token JWTToken
+	c.BindJSON(&token)
 
-  }, [])
+	if token.TokenString == "" {
+		c.String(200, "Couldn't Get Cookie")
+		return
+	}
 
-  return user
+	tokenString := token.TokenString
+	result, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(os.Getenv("SECRETKEY")), nil
+
+	})
+
+	if err != nil {
+		c.String(200, "Token Parsing Failed")
+		return
+	}
+
+	if claims, ok := result.Claims.(jwt.MapClaims); ok && result.Valid {
+
+		if float64(time.Now().Unix()) > claims["expire"].(float64) {
+
+			c.String(200, "Cookie Expired")
+			return
+
+		}
+
+		var user models.User
+		config.DB.First(&user, "email = ?", claims["subject"])
+
+		if user.ID == 0 {
+
+			var shop models.Shop
+			config.DB.First(&shop, "shop_email = ?", claims["subject"])
+
+			if shop.ID == 0 {
+				c.String(200, "Email Not Found")
+				return
+			} else {
+
+				c.Set("user", shop)
+				c.Next()
+
+			}
+
+		} else {
+
+			c.Set("user", user)
+			c.Next()
+
+		}
+
+	} else {
+		c.String(200, "Server Error")
+	}
 
 }
 
-export default useAuth;
