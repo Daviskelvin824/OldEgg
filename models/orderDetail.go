1f7b39538d41a7f5b3f47a9a58203f17dac00f4b
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-12-01 10:24:54 +0700

    Create orderDetail.go

---
+++ models/orderDetail.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type OrderDetail struct {
+	gorm.Model
+	OrderHeaderID uint   `json:"order_header_id" gorm:"References:order_headers(ID)"`
+	ProductID     string `json:"product_id" gorm:"References:products(product_id)"`
+	Quantity      int    `json:"quantity"`
+	Status        string `json:"status"`
+}
