4acb0b0c8b6c688c942311bb5d27d448c531f72c
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-12-01 10:25:32 +0700

    Create productCategory.go

---
+++ models/productCategory.go
@@ -0,0 +1,9 @@
+package models
+
+import "gorm.io/gorm"
+
+type ProductCategory struct {
+	gorm.Model
+	ProductCategoryID   int    `json:"product_category_id" gorm:"primary_key"`
+	ProductCategoryName string `json:"product_category_name"`
+}
