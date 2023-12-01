79dd6fe24129f2964b95fc36c6e6845a22c33d41
Daviskelvin824 <151156638+Daviskelvin824@users.noreply.github.com>
2023-12-01 10:29:04 +0700

    Update UserRouter.go

--- route/UserRouter.go
+++ route/UserRouter.go
@@ -14,4 +14,9 @@ func UserRoute(router *gin.Engine) {
 	router.POST("/subscribe-to-newsletter", controller.SubscribeToNewsletter)
 	router.POST("/request-two-factor-authentication-code", controller.RequestTFACode)
 	router.POST("/change-password", controller.ChangePassword)
+	router.POST("/sign-in-with-one-time-code", controller.SignInWithOneTimeCode)
+	router.POST("/get-one-time-sign-in-code", controller.GetOneTimeSignInCode)
+	router.POST("/request-forgot-password-code", controller.RequestForgotPasswordCode)
+	router.POST("/validate-forgot-password-code", controller.ValidateForgotPasswordCode)
+	router.POST("/reset-password", controller.ResetPassword)
 }
